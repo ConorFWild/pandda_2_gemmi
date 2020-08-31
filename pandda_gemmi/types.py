df5af4870864fe21b9f163be9855ba14a8cc36d6
ConorFWild <41680328+ConorFWild@users.noreply.github.com>
2020-08-31 15:43:02 +0100

    tests

--- pandda_gemmi/types.py
+++ pandda_gemmi/types.py
@@ -798,6 +798,9 @@ class Model:
         # TODO: Make sure this is actually equivilent
         # Calculated from slope of array - mean distribution against normal(0,1)
         residual = array - mean
+        print(np.allclose(array, mean))
+        print(np.allclose(array, np.zeros(mean.shape)))
+        print(np.allclose(mean, np.zeros(mean.shape)))
 
         print("array")
         print(np.sum(np.isnan(residual)))
