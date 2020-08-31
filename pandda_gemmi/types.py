e7b037b728211f3d8feb9212fa68a1470386bf07
ConorFWild <41680328+ConorFWild@users.noreply.github.com>
2020-08-31 15:25:02 +0100

    Update types.py

--- pandda_gemmi/types.py
+++ pandda_gemmi/types.py
@@ -827,7 +827,7 @@ class Model:
     def vectorised_optimisation_bf(func, start, stop, num, shape):
         xs = np.linspace(start, stop, num)
 
-        val = np.ones(shape) * xs[0]
+        val = np.ones(shape) * xs[0] + 1.0/100000000.0
 
         y_max = func(val)
 
