cbb46da516938ae6a8b7dfdcc3f76b28e4200875
ConorFWild <41680328+ConorFWild@users.noreply.github.com>
2020-09-24 16:44:11 +0100

    Update types.py

--- pandda_gemmi/types.py
+++ pandda_gemmi/types.py
@@ -1511,7 +1511,7 @@ class Clustering:
         point_111 = grid.grid.get_point(1, 1, 1)
         position_000 = grid.grid.point_to_position(point_000)
         position_111 = grid.grid.point_to_position(point_111)
-        clustering_cutoff = position_000.dist(position_111)
+        clustering_cutoff = position_000.dist(position_111)*1.25
         print("\tClustering cutoff is: {}".format(clustering_cutoff))
 
         print("\tClustering")
