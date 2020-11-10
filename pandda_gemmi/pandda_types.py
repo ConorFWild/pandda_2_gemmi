bcc0a0de5429578f67d4a44c62c175085ff485fb
ConorFWild <41680328+ConorFWild@users.noreply.github.com>
2020-11-10 17:37:57 +0000

    t

--- pandda_gemmi/pandda_types.py
+++ pandda_gemmi/pandda_types.py
@@ -3528,14 +3528,17 @@ class Clusterings:
                 centroid = (centroid_array[0],
                                       centroid_array[1],
                                       centroid_array[2], )                
-                event_mask_indicies  = (np.concatenate([current_cluster.event_mask_indicies[i] 
-                                                    for current_cluster
-                                                    in current_clusters
-                                                    ], axis=None,
-                                                    )
-                                        for i
-                                        in [1, 2, 3]
-                                        )
+                event_mask_indicies  = (
+                    np.concatenate(
+                        [current_cluster.event_mask_indicies[i] 
+                         for current_cluster
+                         in current_clusters
+                         ], 
+                        axis=None,
+                        )
+                    for i
+                    in [1, 2, 3]
+                    )
                 
                 new_cluster = Cluster(
                     cluster_points_tuple,
