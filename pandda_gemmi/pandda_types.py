610b7b628051f04a05d479a79b46c277068e408a
ConorFWild <41680328+ConorFWild@users.noreply.github.com>
2021-01-14 14:42:25 +0000

    t

--- pandda_gemmi/pandda_types.py
+++ pandda_gemmi/pandda_types.py
@@ -2927,6 +2927,9 @@ class Xmaps:
             
             keys = list(datasets.datasets.keys())
             
+            print(alignments)
+            print(grid)
+            
             results = mapper(
                                            delayed(Xmap.from_unaligned_dataset_c)(
                                                datasets[key],
