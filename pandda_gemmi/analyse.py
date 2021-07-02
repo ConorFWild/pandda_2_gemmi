5deabea300e04e8a7085c23d49dcc8400551b8f8
cwild <conor.wild@sky.com>
2021-07-02 21:24:07 +0100

    y

--- pandda_gemmi/analyse.py
+++ pandda_gemmi/analyse.py
@@ -378,7 +378,7 @@ def process_shell(
         process_local=process_local_serial,
     )
 
-    results = process_local(
+    results = process_local_serial(
         [
             partial(
                 process_dataset_paramaterized,
