15a2236db2b9311f472db7d485e6ab370511c39f
cwild <conor.wild@sky.com>
2021-07-14 11:17:47 +0100

    y

--- tests/test_cut_maps.py
+++ tests/test_cut_maps.py
@@ -4,17 +4,18 @@ unit_cell = gemmi.UnitCell(10, 10, 10, 90, 90, 90)
 grid = gemmi.FloatGrid(100, 100, 100)
 grid.fill(1.0)
 grid.set_unit_cell(unit_cell)
-
-box = gemmi.PositionBox()
-box.minimum = gemmi.Position(-2.0, 2.0, 2.5)
-box.maximum = gemmi.Position(2.5, 7.5, 7.5)
-
-ccp4 = gemmi.Ccp4Map()
-ccp4.update_ccp4_header(2, True)
-ccp4.grid = grid
-ccp4.setup()
-ccp4.set_extent(box)
-ccp4.write_ccp4_map("position.ccp4")
+grid.spacegroup = gemmi.find_spacegroup_by_name("P 1")
+#
+# box = gemmi.PositionBox()
+# box.minimum = gemmi.Position(-2.0, 2.0, 2.5)
+# box.maximum = gemmi.Position(2.5, 7.5, 7.5)
+#
+# ccp4 = gemmi.Ccp4Map()
+# ccp4.update_ccp4_header(2, True)
+# ccp4.grid = grid
+# ccp4.setup()
+# ccp4.set_extent(box)
+# ccp4.write_ccp4_map("position.ccp4")
 
 box = gemmi.FractionalBox()
 box.minimum = gemmi.Fractional(-0.25, 0.25, 0.25)
