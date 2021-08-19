8b44b4179f73708526084b36ca3a7a9f7ae23b0a
cwild <conor.wild@sky.com>
2021-08-19 13:39:25 +0100

    y

---
+++ tests/rename_pdb_and_mtzs.py
@@ -0,0 +1,32 @@
+import os
+from pathlib import Path
+
+import fire
+
+
+def rename(directory: str, dry=True):
+    directory = Path(directory)
+
+    for path in directory.rglob("*"):
+
+        if path.suffix == ".pdb":
+            new_path = path.parent / f"{path.parent.name}.pdb"
+            if dry:
+                print(f"Would rename {path} to {new_path}")
+            else:
+                path.rename(new_path)
+
+        elif path.suffix == ".mtz":
+            new_path = path.parent / f"{path.parent.name}.mtz"
+
+            if dry:
+                print(f"Would rename {path} to {new_path}")
+            else:
+                path.rename(new_path)
+
+        else:
+            continue
+
+
+if __name__ == "__main__":
+    fire.Fire(rename)
