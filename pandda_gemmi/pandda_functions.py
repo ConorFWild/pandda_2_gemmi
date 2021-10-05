36c12bcfda10b1880355768ec71906e2252f63be
cwild <conor.wild@sky.com>
2021-10-05 12:15:16 +0100

    hdbsc

--- pandda_gemmi/pandda_functions.py
+++ pandda_gemmi/pandda_functions.py
@@ -993,6 +993,7 @@ def get_clusters_nn(
         dtag_distance_to_cluster[_dtag] = {}
         dtag_coord = reduced_array[dtag_index, :].reshape((1, -1))
         for cluster, cluster_median in cluster_medians.items():
+            print([cluster_median.shape, dtag_coord.shape])
             assert cluster_median.shape[0] == dtag_coord[0]
             assert cluster_median.shape[1] == dtag_coord[1]
             distance = np.linalg.norm(cluster_median - dtag_coord)
