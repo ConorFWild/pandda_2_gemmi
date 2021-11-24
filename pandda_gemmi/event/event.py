5ccb2f7d66bd371042cc1e1660a245f2b229280e
cwild <conor.wild@sky.com>
2021-11-24 14:32:42 +0000

    Significant refactor

--- pandda_gemmi/event/event.py
+++ pandda_gemmi/event/event.py
@@ -9,13 +9,91 @@ from sklearn.cluster import DBSCAN
 from joblib.externals.loky import set_loky_pickler
 set_loky_pickler('pickle')
 
-from pandda_gemmi.pandda_functions import save_event_map
+# from pandda_gemmi.pandda_functions import save_event_map
 from pandda_gemmi.python_types import *
 from pandda_gemmi.common import EventIDX, EventID, SiteID, Dtag, PositionsArray, delayed
-from pandda_gemmi.dataset import Reference
+from pandda_gemmi.dataset import Reference, Dataset, StructureFactors
 from pandda_gemmi.edalignment import Grid, Xmap, Alignment, Xmaps, Partitioning
 from pandda_gemmi.model import Zmap, Zmaps, Model
 from pandda_gemmi.sites import Sites
+
+def save_event_map(
+        path,
+         xmap: Xmap,
+         model: Model,
+         event: Event,
+         dataset: Dataset,
+         alignment: Alignment,
+         grid: Grid,
+         structure_factors: StructureFactors,
+         mask_radius: float,
+         mask_radius_symmetry: float,
+         partitioning: Partitioning,
+         sample_rate: float,
+         ):
+    reference_xmap_grid = xmap.xmap
+    reference_xmap_grid_array = np.array(reference_xmap_grid, copy=True)
+
+    # moving_xmap_grid: gemmi.FloatGrid = dataset.reflections.reflections.transform_f_phi_to_map(structure_factors.f,
+    #                                                                                          structure_factors.phi,
+    #                                                                                          )
+
+    event_map_reference_grid = gemmi.FloatGrid(*[reference_xmap_grid.nu,
+                                                 reference_xmap_grid.nv,
+                                                 reference_xmap_grid.nw,
+                                                 ]
+                                               )
+    event_map_reference_grid.spacegroup = gemmi.find_spacegroup_by_name("P 1")  # xmap.xmap.spacegroup
+    event_map_reference_grid.set_unit_cell(reference_xmap_grid.unit_cell)
+
+    event_map_reference_grid_array = np.array(event_map_reference_grid,
+                                              copy=False,
+                                              )
+
+    mean_array = model.mean
+    event_map_reference_grid_array[:, :, :] = (reference_xmap_grid_array - (event.bdc.bdc * mean_array)) / (
+            1 - event.bdc.bdc)
+
+    event_map_grid = Xmap.from_aligned_map_c(
+        event_map_reference_grid,
+        dataset,
+        alignment,
+        grid,
+        structure_factors,
+        mask_radius,
+        partitioning,
+        mask_radius_symmetry,
+        sample_rate
+    )
+
+    # # # Get the event bounding box
+    # # Find the min and max positions
+    # min_array = np.array(event.native_positions[0])
+    # max_array = np.array(event.native_positions[0])
+    # for position in event.native_positions:
+    #     position_array = np.array(position)
+    #     min_array = np.min(np.vstack(min_array, position_array), axis=0)
+    #     max_array = np.max(np.vstack(max_array, position_array), axis=0)
+    #
+    #
+    # # Get them as fractional bounding box
+    # print(min_array)
+    # print(max_array)
+    # print(event.native_positions[0])
+    # print(event.native_centroid)
+    # print(event.cluster.centroid)
+    #
+    # box = gemmi.FractionalBox()
+    # box.minimum = gemmi.Fractional(min_array[0], min_array[1], min_array[2])
+    # box.maximum = gemmi.Fractional(max_array[0], max_array[1], max_array[2])
+
+    ccp4 = gemmi.Ccp4Map()
+    ccp4.grid = event_map_grid.xmap
+    ccp4.update_ccp4_header(2, True)
+    ccp4.setup()
+    # ccp4.set_extent(box)
+    # ccp4.grid.symmetrize_max()
+    ccp4.write_ccp4_map(str(path))
 
 
 @dataclasses.dataclass()
