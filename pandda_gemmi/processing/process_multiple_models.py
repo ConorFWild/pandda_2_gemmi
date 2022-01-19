7f5e450ceed23ce7fbfc1777c47436af97a57e1e
cwild <conor.wild@sky.com>
2022-01-19 11:46:28 +0000

    Working on using fragment to pre-score clusters

--- pandda_gemmi/processing/process_multiple_models.py
+++ pandda_gemmi/processing/process_multiple_models.py
@@ -1100,7 +1100,12 @@ def process_shell_multiple_models(
     )
 
     # Process each dataset in the shell
-    all_train_dtags = [_dtag for l in shell.train_dtags.values() for _dtag in l]
+    all_train_dtags_unmerged = [_dtag for l in shell.train_dtags.values() for _dtag in l]
+    all_train_dtags = []
+    for _dtag in all_train_dtags_unmerged:
+        if not _dtag in all_train_dtags:
+            all_train_dtags.append(_dtag)
+
     if debug:
         print(f"\tAll train datasets are: {all_train_dtags}")
     # dataset_dtags = {_dtag:  for _dtag in shell.test_dtags for n in shell.train_dtags}
