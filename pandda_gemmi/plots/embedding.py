9c0f94f2051774b2429132b45ebf789d39bfc65f
cwild <conor.wild@sky.com>
2022-02-22 17:25:14 +0000

    Working on using fragment to pre-score clusters

--- pandda_gemmi/plots/embedding.py
+++ pandda_gemmi/plots/embedding.py
@@ -2,7 +2,7 @@ import numpy as np
 from sklearn import decomposition, metrics
 import umap
 from bokeh.plotting import ColumnDataSource, figure, output_file, show, save
-from bokeh.palettes import magma
+from bokeh.palettes import magma, viridis
 
 def embed_umap(reduced_array):
     distance_matrix = metrics.pairwise_distances(reduced_array)
@@ -21,7 +21,9 @@ def bokeh_scatter_plot(embedding, labels, known_apos, plot_file):
     unique_annotations = np.unique(known_apos)
     colour_map = {annotation: j for j, annotation in enumerate(unique_annotations)}
     num_unique_annotations = len(unique_annotations)
-    pallette = magma(num_unique_annotations)
+    # pallette = magma(num_unique_annotations)
+    pallette = viridis(num_unique_annotations)
+
     for annotation in known_apos:
         annotations.append(annotation)
         colours.append(pallette[colour_map[annotation]])
