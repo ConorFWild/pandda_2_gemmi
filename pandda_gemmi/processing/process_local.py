d301ae970a5a9cb1f792faaf677ec41752a16d99
cwild <conor.wild@sky.com>
2022-03-30 13:34:21 +0100

    Updated event score interface and added pickling for event scoring testing

--- pandda_gemmi/processing/process_local.py
+++ pandda_gemmi/processing/process_local.py
@@ -10,18 +10,23 @@ class RayWrapper(Generic[P, V]):
     def run(self, func: Callable[P, V], *args: P.args, **kwargs: P.kwargs) -> V:
         return func(*args, **kwargs)
 
+@ray.remote
+def ray_wrapper(func: Callable[P, V], *args: P.args, **kwargs: P.kwargs) -> V
+    return func(*args, **kwargs)
+
 
 class ProcessLocalRay(ProcessorInterface):
 
     def __call__(self, funcs: Iterable[PartialInterface[P, V]]) -> List[V]:
         assert ray.is_initialized() == True
-        actors = [RayWrapper.remote() for f in funcs]
+        # actors = [RayWrapper.remote() for f in funcs]
         # This isn't properly typeable because the wrapper dynamically costructs the run method on the actor
-        print([f for f in funcs])
-        print([f.args for f in funcs])
-        print(f.kwargs for f in funcs)
-        tasks = [a.run.remote(f.func, *f.args, **f.kwargs) for a, f in zip(actors, funcs)]  # type: ignore
-        print(tasks)
+        # print([f for f in funcs])
+        # print([f.args for f in funcs])
+        # print(f.kwargs for f in funcs)
+        # tasks = [a.run.remote(f.func, *f.args, **f.kwargs) for a, f in zip(actors, funcs)]  # type: ignore
+        tasks = [ray_wrapper.remote(f.func, *f.args, **f.kwargs) for f in funcs]
+        # print(tasks)
         results = ray.get(tasks)
         return results
 
