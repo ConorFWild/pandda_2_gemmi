2359d6901582c8985f47a260a5b89085b57014b6
cwild <conor.wild@sky.com>
2022-03-31 10:53:18 +0100

    Adding proper typing

--- tests/test_autobuild.py
+++ tests/test_autobuild.py
@@ -16,7 +16,7 @@ def unpickle(path: Path):
 
 class DtagAutobuildTestDataDir:
     def __init__(self, path: Path):
-        dtag_pickle_path = path / "dataset_dtag.pickle"
+        dtag_pickle_path = path / "dataset_processed_dataset.pickle"
         events_pickle_path = path / "events.pickle"
         dataset_pickle_path = path / "dataset.pickle"
         dataset_xmap_pickle_path = path / "dataset_xmap.pickle"
