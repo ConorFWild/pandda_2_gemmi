3702c3904a48e98949b53e6b45dc1e813e38d31b
cwild <conor.wild@sky.com>
2022-04-03 22:11:27 +0100

    Adding proper typing

--- pandda_gemmi/event/event_scoring_inbuilt.py
+++ pandda_gemmi/event/event_scoring_inbuilt.py
@@ -395,7 +395,7 @@ def score_conformer(cluster: Cluster, conformer, zmap_grid, debug=False):
         lambda params: score_fit(
             probe_structure,
             zmap_grid,
-            6,
+            12,
             params
         ),
         [
