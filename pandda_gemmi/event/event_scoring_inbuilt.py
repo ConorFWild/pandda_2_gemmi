848e617359c6db8dac7f8840577f0a233103e61f
cwild <conor.wild@sky.com>
2022-05-05 12:15:53 +0100

    Abstractions for ligand based event scoring

--- pandda_gemmi/event/event_scoring_inbuilt.py
+++ pandda_gemmi/event/event_scoring_inbuilt.py
@@ -445,7 +445,7 @@ def score_fit_array(structure_array, grid, distance, params):
 
     vals = get_interpolated_values_c(grid, transformed_structure_array, n)
     print(vals)
-    print(f"{np.array(grid) == 1.0}")
+    print(f"{np.sum(np.array(grid) == 1.0)}")
     print(transformed_structure_array)
 
     # print(type(transformed_structure_array))
