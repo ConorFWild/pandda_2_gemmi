da622403644174668e1ff97b3bbbba04e06e1974
cwild <conor.wild@sky.com>
2022-12-16 10:17:17 +0000

    New scatter of data method

--- pandda_gemmi/processing/process_global.py
+++ pandda_gemmi/processing/process_global.py
@@ -98,7 +98,10 @@ class DaskDistributedProcessor(ProcessorInterface):
         # result_futures = self.client.map(run_multiprocessing, func_futures)
         result_futures = []
         for func in funcs:
-            result_futures.append(self.client.submit(func.func, *func.args, **func.kwargs))
+            args = [self.client.persist(arg) for arg in func.args]
+            kwargs = {kwrd: self.client.persist(kwarg) for kwrd, kwarg in func.kwargs.items()}
+            # result_futures.append(self.client.submit(func.func, *func.args, **func.kwargs))
+            result_futures.append(self.client.submit(func.func, *args, **kwargs))
 
         # progress(result_futures)
 
