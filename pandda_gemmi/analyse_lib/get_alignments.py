f7dfdb470e178a5dfecbeeca9eb58cec7b261ab1
jfi22496 <conor.wild@sky.com>
2023-02-17 19:02:23 +0000

    Caching results

--- pandda_gemmi/analyse_lib/get_alignments.py
+++ pandda_gemmi/analyse_lib/get_alignments.py
@@ -3,26 +3,30 @@ import pickle
 from pandda_gemmi.common import update_log
 from pandda_gemmi.analyse_interface import *
 from pandda_gemmi import constants
-from pandda_gemmi.edalignment import GetAlignments
+from pandda_gemmi.edalignment import GetAlignments, Alignments
 
 def get_alignments(pandda_args, console, pandda_log, pandda_fs_model: PanDDAFSModelInterface, datasets, reference):
     console.start_alignments()
 
-    # with STDOUTManager('Getting local alignments of the electron density to the reference...', f'\tDone!'):
-    alignments: AlignmentsInterface = GetAlignments()(
-        reference,
-        datasets,
-    )
+    if len(pandda_fs_model.alignment_files) != 0:
+        alignments = Alignments({dtag: alignment_file.load() for dtag, alignment_file in pandda_fs_model.alignment_files.items()})
 
-    if pandda_args.debug >= Debug.AVERAGE_MAPS:
-        with open(pandda_fs_model.pandda_dir / "alignments.pickle", "wb") as f:
-            pickle.dump(alignments, f)
+    else:
+        # with STDOUTManager('Getting local alignments of the electron density to the reference...', f'\tDone!'):
+        alignments: AlignmentsInterface = GetAlignments()(
+            reference,
+            datasets,
+        )
 
-    update_log(pandda_log, pandda_args.out_dir / constants.PANDDA_LOG_FILE)
+        if pandda_args.debug >= Debug.AVERAGE_MAPS:
+            with open(pandda_fs_model.pandda_dir / "alignments.pickle", "wb") as f:
+                pickle.dump(alignments, f)
 
-    console.summarise_local_alignment()
+        update_log(pandda_log, pandda_args.out_dir / constants.PANDDA_LOG_FILE)
 
-    for dtag, alignment in alignments.alignments.items():
-        pandda_fs_model.alignment_files[dtag].save(alignment)
+        console.summarise_local_alignment()
+
+        for dtag, alignment in alignments.alignments.items():
+            pandda_fs_model.alignment_files[dtag].save(alignment)
 
     return alignments
--- pandda_gemmi/analyse_lib/get_grid.py
+++ pandda_gemmi/analyse_lib/get_grid.py
@@ -6,24 +6,27 @@ from pandda_gemmi.edalignment import GetGrid
 
 def get_grid(pandda_args, console, pandda_fs_model: PanDDAFSModelInterface, reference):
     console.start_get_grid()
-    # Grid
-    # with STDOUTManager('Getting the analysis grid...', f'\tDone!'):
-    grid: GridInterface = GetGrid()(reference,
-                                    pandda_args.outer_mask,
-                                    pandda_args.inner_mask_symmetry,
-                                    # sample_rate=pandda_args.sample_rate,
-                                    sample_rate=reference.dataset.reflections.get_resolution() / 0.5,
-                                    debug=pandda_args.debug
-                                    )
-    if pandda_args.debug >= Debug.AVERAGE_MAPS:
-        with open(pandda_fs_model.pandda_dir / "grid.pickle", "wb") as f:
-            pickle.dump(grid, f)
+    if pandda_fs_model.grid_file.path.exists():
+        grid = pandda_fs_model.grid_file.load()
+    else:
+        # Grid
+        # with STDOUTManager('Getting the analysis grid...', f'\tDone!'):
+        grid: GridInterface = GetGrid()(reference,
+                                        pandda_args.outer_mask,
+                                        pandda_args.inner_mask_symmetry,
+                                        # sample_rate=pandda_args.sample_rate,
+                                        sample_rate=reference.dataset.reflections.get_resolution() / 0.5,
+                                        debug=pandda_args.debug
+                                        )
+        if pandda_args.debug >= Debug.AVERAGE_MAPS:
+            with open(pandda_fs_model.pandda_dir / "grid.pickle", "wb") as f:
+                pickle.dump(grid, f)
 
-        grid.partitioning.save_maps(
-            pandda_fs_model.pandda_dir
-        )
+            grid.partitioning.save_maps(
+                pandda_fs_model.pandda_dir
+            )
 
-    pandda_fs_model.grid_file.save(grid)
+        pandda_fs_model.grid_file.save(grid)
 
-    console.summarise_get_grid(grid)
+        console.summarise_get_grid(grid)
     return grid
