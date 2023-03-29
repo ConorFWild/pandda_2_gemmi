c7d712e5c6c1115f56d533260ce8cd99f767e17b
jfi22496 <conor.wild@sky.com>
2023-03-29 20:47:14 +0100

    draft pandda 2.1

--- pandda_gemmi/scratch/dataset/dataset.py
+++ pandda_gemmi/scratch/dataset/dataset.py
@@ -4,6 +4,6 @@ from .reflections import Reflections
 
 class XRayDataset:
     def __init__(self, structure_path, reflections_path, ligand_files):
-        self.structure = Structure(structure_path)
+        self.structure = Structure.from_path(structure_path)
         self.reflections = Reflections.from_path(reflections_path)
         self.ligand_files = ligand_files
