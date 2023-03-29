77817c2b8e02587dce2bb3417616be4f2ebeabf4
jfi22496 <conor.wild@sky.com>
2023-03-29 20:38:08 +0100

    draft pandda 2.1

--- pandda_gemmi/scratch/fs/__init__.py
+++ pandda_gemmi/scratch/fs/__init__.py
@@ -1,3 +1,3 @@
-from pandda_fs import PanDDAFS
-from pandda_input import PanDDAInput
-from pandda_output import PanDDAOutput
+from .pandda_fs import PanDDAFS
+from .pandda_input import PanDDAInput
+from .pandda_output import PanDDAOutput
