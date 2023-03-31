72b29391802719b0baf3a4ec1f9867a6a25e9555
jfi22496 <conor.wild@sky.com>
2023-03-31 12:33:26 +0100

    draft pandda 2.1

--- pandda_gemmi/scratch/tests/test_sparse_dmap_stream.py
+++ pandda_gemmi/scratch/tests/test_sparse_dmap_stream.py
@@ -51,7 +51,7 @@ def test_sparse_dmap_stream(data_dir, out_dir):
     finish_get_frame = time.time()
     print(f"Got reference frame in {round(finish_get_frame-begin_get_frame, 1)}")
     for resid, partition in reference_frame.partitioning.partitions.items():
-        print(f"\tResid: {resid} : {len(partition)}")
+        print(f"\tResid: {resid} : {partition.points.shape}")
 
     # Get the dmaps
     print(f"##### Getting sparse dmap loader #####")
