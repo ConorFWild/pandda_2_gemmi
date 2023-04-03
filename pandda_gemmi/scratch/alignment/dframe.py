e8c0f7a08861bb633ac6202934ccdaeecb61205d
jfi22496 <conor.wild@sky.com>
2023-04-03 17:45:46 +0100

    draft pandda 2.1

--- pandda_gemmi/scratch/alignment/dframe.py
+++ pandda_gemmi/scratch/alignment/dframe.py
@@ -183,7 +183,7 @@ class PointPositionArray(PointPositionArrayInterface):
         point_arrays = []
         position_arrays = []
         for j in range(position_array.shape[0]):
-            grid = np.mgrid[u0: u1 + 1, v0: v1 + 1, w0: w1 + 1]
+            grid = np.mgrid[u0[j]: u1[j] + 1, v0[j]: v1[j] + 1, w0[j]: w1[j] + 1]
             grid_point_array = grid.reshape((3, -1)).T
         #     np.array(
         #     [
