ea52e91b3a82519ee3fc2d7f9fa0549713610f21
jfi22496 <conor.wild@sky.com>
2023-04-03 18:51:16 +0100

    draft pandda 2.1

--- pandda_gemmi/scratch/alignment/dframe.py
+++ pandda_gemmi/scratch/alignment/dframe.py
@@ -116,9 +116,9 @@ class PointPositionArray(PointPositionArrayInterface):
         position_array = PointPositionArray.orthogonalize_fractional_array(
             PointPositionArray.fractionalize_grid_point_array(
                 grid_point_array,
-                grid,
+                np.array(grid.unit_cell.orthogonalization_matrix.tolist()),
             ),
-            grid,
+            [grid.nu, grid.nv, grid.nw],
         )
         # print(f"Grid position array shape: {position_array.shape}")
         # print(f"Grid position first element: {position_array[0, :]}")
