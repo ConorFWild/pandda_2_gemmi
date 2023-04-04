72cb9d6024ee1a2a3b428c2b843cc4f3cc054def
jfi22496 <conor.wild@sky.com>
2023-04-04 18:02:14 +0100

    draft pandda 2.1

--- pandda_gemmi/scratch/processor/processor_ray.py
+++ pandda_gemmi/scratch/processor/processor_ray.py
@@ -42,6 +42,7 @@ class ProcessLocalRay(ProcessorInterface):
         return results
 
 
+
     def process_dict(self, funcs):
         assert ray.is_initialized() == True
         tasks = [ray_wrapper.remote(f.func, *f.args, **f.kwargs) for f in funcs.values()]
