1fdb52a8a67b222d9937b30ae41f70ca71f0e15f
jfi22496 <conor.wild@sky.com>
2023-04-05 18:58:02 +0100

    draft pandda 2.1

--- pandda_gemmi/scratch/tests/test_sparse_dmap_stream.py
+++ pandda_gemmi/scratch/tests/test_sparse_dmap_stream.py
@@ -301,6 +301,7 @@ def test_sparse_dmap_stream(data_dir, out_dir):
         dataset_masked = XRayDataset(dataset.structure, dataset_reflections, dataset.ligand_files)
         mean_dataset_masked = XRayDataset(dataset.structure, mean_reflections, dataset.ligand_files)
         mean_smoothed_grid = SmoothReflections(dataset_masked)(mean_dataset_masked).reflections.transform_f_phi_to_map()
+        print(f"Mean smoothed shape: {np.array(mean_smoothed_grid).shape} vs original {np.array(reference_frame.unmask(SparseDMap(array[0,:].flatten()))).shape}")
         save_dmap(
             mean_smoothed_grid,
             Path(out_dir) / f"{dtag}_mean_smoothed.ccp4"
