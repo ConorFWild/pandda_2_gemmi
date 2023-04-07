ebed607466ece74f459b29704b096401d5a6c5d3
jfi22496 <conor.wild@sky.com>
2023-04-07 11:05:54 +0100

    “

--- pandda_gemmi/scratch/tests/test_sparse_dmap_stream.py
+++ pandda_gemmi/scratch/tests/test_sparse_dmap_stream.py
@@ -24,6 +24,107 @@ from pandda_gemmi.scratch.alignment import Alignment, DFrame
 from pandda_gemmi.scratch.processor import ProcessLocalRay, Partial
 from pandda_gemmi.scratch.dataset import ResidueID
 
+def sample_xmap(xmap, transform, sample_array):
+    xmap.interpolate_values(sample_array, transform)
+    return sample_array
+
+
+def get_sample_transform_from_event(centroid,
+                                    sample_distance: float,
+                                    n: int,
+                                    translation: float):
+    # Get basic sample grid transform
+    initial_transform = gemmi.Transform()
+    scale_matrix = np.eye(3) * sample_distance
+    initial_transform.mat.fromlist(scale_matrix.tolist())
+
+    # Get sample grid centroid
+    sample_grid_centroid = (np.array([n, n, n]) * sample_distance) / 2
+
+    # Get centre grid transform
+    centre_grid_transform = gemmi.Transform()
+    centre_grid_transform.vec.fromlist([
+        -sample_grid_centroid[0],
+        -sample_grid_centroid[1],
+        -sample_grid_centroid[2],
+    ])
+
+    # Event centre transform
+    event_centre_transform = gemmi.Transform()
+    event_centre_transform.vec.fromlist([x for x in centroid])
+
+    # Apply random translation
+    # transform = event_centre_transform.combine(
+    #     centre_grid_transform.combine(
+    #         initial_transform
+    #     )
+    # )
+
+    transform = gemmi.Transform()
+    transform.vec.fromlist([
+        event.cluster.centroid[j] - sample_grid_centroid[j]
+        for j
+        in [0,1,2]
+    ])
+    transform.mat.fromlist(scale_matrix.tolist())
+    corner_0_pos = transform.apply(gemmi.Position(0.0, 0.0, 0.0))
+    corner_n_pos = transform.apply(gemmi.Position(
+        float(n),
+        float(n),
+        float(n),
+    )
+    )
+    corner_0 = (corner_0_pos.x, corner_0_pos.y, corner_0_pos.z)
+    corner_n = (corner_n_pos.x, corner_n_pos.y, corner_n_pos.z)
+    average_pos = [c0 + (cn - c0) / 2 for c0, cn in zip(corner_0, corner_n)]
+    event_centroid = event.cluster.centroid
+    # logger.debug(f"Centroid: {event_centroid}")
+    # logger.debug(f"Corners: {corner_0} : {corner_n} : average: {average_pos}")
+    # logger.debug(f"Distance from centroid to average: {gemmi.Position(*average_pos).dist(gemmi.Position(*event_centroid))}")
+
+    return transform
+
+
+def get_model_map(structure, xmap_event):
+
+    # structure = reference.dataset.structure.structure
+    new_xmap = gemmi.FloatGrid(xmap_event.nu, xmap_event.nv, xmap_event.nw)
+    new_xmap.spacegroup = xmap_event.spacegroup
+    new_xmap.set_unit_cell(xmap_event.unit_cell)
+    for model in structure:
+        for chain in model:
+            for residue in chain.get_polymer():
+                for atom in residue:
+                    new_xmap.set_points_around(
+                        atom.pos,
+                        radius=1,
+                        value=1.0,
+                    )
+
+    return new_xmap
+
+
+# def get_event_map(reference_xmap_grid, event: EventInterface, model: ModelInterface):
+#     reference_xmap_grid_array = np.array(reference_xmap_grid, copy=True)
+#
+#     event_map_reference_grid = gemmi.FloatGrid(*[reference_xmap_grid.nu,
+#                                                  reference_xmap_grid.nv,
+#                                                  reference_xmap_grid.nw,
+#                                                  ]
+#                                                )
+#     event_map_reference_grid.spacegroup = gemmi.find_spacegroup_by_name("P 1")  # xmap.xmap.spacegroup
+#     event_map_reference_grid.set_unit_cell(reference_xmap_grid.unit_cell)
+#
+#     event_map_reference_grid_array = np.array(event_map_reference_grid,
+#                                               copy=False,
+#                                               )
+#
+#     mean_array = model.mean
+#     event_map_reference_grid_array[:, :, :] = (reference_xmap_grid_array - (event.bdc.bdc * mean_array)) / (
+#             1 - event.bdc.bdc)
+#
+#     return event_map_reference_grid
+
 def save_dmap(dmap, path):
     ccp4 = gemmi.Ccp4Map()
     ccp4.grid = dmap
@@ -907,10 +1008,95 @@ def test_sparse_dmap_stream(data_dir, out_dir):
 
         cluster_nums, counts = np.unique(clusters, return_counts=True)
 
+        import os
+        import inspect
+
+        import torch
+        import numpy as np
+        import gemmi
+
+        from pandda_gemmi.cnn import resnet
+        from pandda_gemmi.cnn import resnet18
+
+        if torch.cuda.is_available():
+            dev = "cuda:0"
+        else:
+            dev = "cpu"
+
+        # Load the model
+        cnn = resnet18(num_classes=2, num_input=4)
+        cnn_path = Path(os.path.dirname(inspect.getfile(resnet))) / "model.pt"
+        cnn.load_state_dict(torch.load(cnn_path, map_location=dev))
+
+        # Add model to device
+        cnn.to(dev)
+        cnn.eval()
+
+        # Annotate the events
+        event_scores = {}
+        # for event_id, event in events.events.items():
+
+        xmap_grid = reference_frame.unmask(SparseDMap(dtag_array))
+        mean_grid = reference_frame.unmask(SparseDMap(mean))
+
         for cluster_num, count in zip(cluster_nums, counts):
             volume = count * (z_grid.unit_cell.volume / grid.point_count)
             if volume > 5.0:
                 print(f"\tCluster: {cluster_num} : {np.mean(high_z_pos_array[clusters == cluster_num, :], axis=0)} : size: {count}: vol {volume}")
+
+                centroid = np.mean(high_z_pos_array[clusters == cluster_num, :], axis=0)
+
+                n = 30
+                sample_array = np.zeros((n, n, n), dtype=np.float32)
+                sample_transform = get_sample_transform_from_event(
+                    centroid,
+                    0.5,
+                    n,
+                    3.5
+                )
+                sample_array = np.zeros((n, n, n), dtype=np.float32)
+
+                bdcs = np.linspace(0.0,0.95,20).reshape((20,1,1))
+                xmap_sample = sample_xmap(xmap_grid, sample_transform, np.copy(sample_array))
+                mean_map_sample = sample_xmap(mean_grid, sample_transform, np.copy(sample_array))
+
+                image_events = (xmap_sample[np.newaxis, :] - (bdcs * mean_map_sample[np.newaxis, :])) / (1-bdcs)
+
+
+                # event_map = get_event_map(dataset_xmap.xmap, event, model)
+                # sample_array_event = np.copy(sample_array)
+                # image_event = sample_xmap(event_map, sample_transform, sample_array_event)
+
+                # sample_array_raw = np.copy(sample_array)
+                # image_raw = sample_xmap(dataset_xmap.xmap, sample_transform, sample_array_raw)
+                image_raw = np.stack([xmap_sample for _j in range(20)])
+
+                sample_array_zmap = np.copy(sample_array)
+                zmap_sample = sample_xmap(z_grid, sample_transform, sample_array_zmap)
+                image_zmap = np.stack([zmap_sample for _j in range(20)])
+
+                sample_array_model = np.copy(sample_array)
+                model_map = get_model_map(dataset.structure.structure, grid_array)
+                model_sample = sample_xmap(model_map, sample_transform, sample_array_model)
+                image_model = np.stack([model_sample for _j in range(20)])
+
+                image = np.stack([image_events, image_raw, image_zmap, image_model])
+
+                # Transfer to tensor
+                image_t = torch.unsqueeze(torch.from_numpy(image), 0)
+
+                # Move tensors to device
+                image_c = image_t.to(dev)
+
+                # Run model
+                model_annotation = cnn(image_c)
+
+                # Track score
+                model_annotations = model_annotation.to(torch.device("cpu")).detach().numpy()
+                for _j in range(20):
+                    bdc = bdcs[_j]
+                    annotation = model_annotations[_j, 1]
+                    print(f"\t\t{bdc} {annotation}")
 
 
         for bdc in [0.1,0.2,0.3,0.4,0.5,0.6,0.7,0.8,0.9]:
