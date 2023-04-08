36b37556ed9a84368cde33e23cefefd35419ba83
jfi22496 <conor.wild@sky.com>
2023-04-08 17:52:07 +0100

    “

--- pandda_gemmi/scratch/alignment/dframe.py
+++ pandda_gemmi/scratch/alignment/dframe.py
@@ -657,6 +657,8 @@ class PointPositionArray(PointPositionArrayInterface):
         print(f"\t\t\t\t\t With shapes {unique_points.shape} {unique_positions.shape}")
         print(f"\t\t\t\t\t Vs mask size {indicies[0].shape}")
 
+        return unique_points, unique_positions
+
         # all_point_array = grid
 
 
