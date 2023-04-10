b547b28b378d9d20b9e97ff09f6146df8688b39a
jfi22496 <conor.wild@sky.com>
2023-04-10 13:59:20 +0100

    “

--- pandda_gemmi/scratch/tests/test_sparse_dmap_stream.py
+++ pandda_gemmi/scratch/tests/test_sparse_dmap_stream.py
@@ -29,7 +29,7 @@ from pandda_gemmi.cnn import resnet18
 
 from pandda_gemmi.scratch.interfaces import *
 from pandda_gemmi.scratch.fs import PanDDAFS
-from pandda_gemmi.scratch.dataset import XRayDataset, Reflections
+from pandda_gemmi.scratch.dataset import XRayDataset, Reflections, StructureArray
 from pandda_gemmi.scratch.dmaps import DMap, SparseDMap, SparseDMapStream, TruncateReflections, SmoothReflections
 from pandda_gemmi.scratch.alignment import Alignment, DFrame
 from pandda_gemmi.scratch.processor import ProcessLocalRay, Partial
@@ -250,7 +250,10 @@ def test_sparse_dmap_stream(data_dir, out_dir):
     begin_align = time.time()
     # alignments: Dict[str, Alignment] = {_dtag: Alignment(datasets[_dtag], dataset) for _dtag in datasets}
     alignments: Dict[str, Alignment] = processor.process_dict(
-        {_dtag: Partial(Alignment.from_structure_arrays).paramaterise(datasets[_dtag].structure, dataset.structure) for _dtag in datasets_resolution}
+        {_dtag: Partial(Alignment.from_structure_arrays).paramaterise(
+            StructureArray.from_structure(datasets[_dtag].structure),
+            StructureArray.from_structure(dataset.structure),
+        ) for _dtag in datasets_resolution}
     )
     finish_align = time.time()
     print(f"Got {len(alignments)} alignments in {round(finish_align - begin_align, 1)}")
