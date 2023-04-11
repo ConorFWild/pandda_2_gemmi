4b795adfdf0ad1bb9099bd88fea88f4547dc1289
jfi22496 <conor.wild@sky.com>
2023-04-11 19:47:54 +0100

    New pandda

--- pandda_gemmi/scratch/fs/pandda_output.py
+++ pandda_gemmi/scratch/fs/pandda_output.py
@@ -4,14 +4,20 @@ from pathlib import Path
 from .pandda_input import PanDDAInput
 from .. import constants
 
+def try_make(path):
+    try:
+        os.mkdir(path)
+    except:
+        return
 
 class PanDDAOutput:
     def __init__(self, path: Path, pandda_input: PanDDAInput):
         self.path = path
         self.processed_datasets_dir = path / constants.PANDDA_PROCESSED_DATASETS_DIR
-        os.mkdir(self.processed_datasets_dir)
+        try_make(self.processed_datasets_dir)
+
         self.processed_datasets = {}
         for dtag, dataset_dir in pandda_input.dataset_dirs.items():
             processed_dataset_dir = dataset_dir.path / dtag
-            os.mkdir(processed_dataset_dir)
+            try_make(processed_dataset_dir)
             self.processed_datasets[dtag] = processed_dataset_dir
