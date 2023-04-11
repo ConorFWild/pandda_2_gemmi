a0c2c3263ea65b34dba278a05aee22ffb99a1d51
jfi22496 <conor.wild@sky.com>
2023-04-11 18:28:28 +0100

    New pandda

--- pandda_gemmi/scratch/pandda.py
+++ pandda_gemmi/scratch/pandda.py
@@ -166,6 +166,7 @@ def pandda(args: PanDDAArgs):
 
             # Initial
             events = ClusterDensityDBSCAN()(z, reference_frame)
+            print(f"Initial num events: {len(events)}")
 
             # Filter the events pre-scoring
             for filter in [FilterSize(reference_frame, min_size=5.0), FilterCluster(5.0), ]:
