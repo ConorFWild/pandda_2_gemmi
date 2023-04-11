7a770795e0be4dd1cfe6b1c6fc929a6734d393ce
jfi22496 <conor.wild@sky.com>
2023-04-11 19:43:31 +0100

    New pandda

--- pandda_gemmi/scratch/event_model/output.py
+++ pandda_gemmi/scratch/event_model/output.py
@@ -1,8 +1,38 @@
+import gemmi
+
+from ..interfaces import *
+from .. import constants
+from ..dmaps import SparseDMap
+
 def output_models(fs, characterization_sets, selected_model_num):
     ...
 
 def output_events(fs, model_events):
     ...
 
-def output_maps(fs, selected_events):
-    ...
+
+def save_dmap(dmap, path):
+    ccp4 = gemmi.Ccp4Map()
+    ccp4.grid = dmap
+    ccp4.update_ccp4_header(2, True)
+    ccp4.write_ccp4_map(str(path))
+
+def output_maps(
+        fs,
+        selected_events: Dict[Tuple[str, int], EventInterface],
+        dtag_array,
+        selected_mean,
+        reference_frame
+):
+    for event_id, event in selected_events.items():
+        event_array = (dtag_array - (event.bdc * selected_mean)) / (1 - event.bdc)
+        event_grid = reference_frame.unmask(SparseDMap(event_array))
+
+        save_dmap(
+            event_grid,
+            Path(fs.output.processed_datasets[event_id[0]]) / constants.PANDDA_EVENT_MAP_FILE.format(
+                event_id[0],
+                event_id[1],
+                round(event.bdc, 2)
+            )
+        )
--- pandda_gemmi/scratch/fs/pandda_output.py
+++ pandda_gemmi/scratch/fs/pandda_output.py
@@ -1,5 +1,16 @@
+import os
 from pathlib import Path
 
+from .pandda_input import PanDDAInput
+from .. import constants
+
+
 class PanDDAOutput:
-    def __init__(self, path: Path):
+    def __init__(self, path: Path, pandda_input: PanDDAInput):
         self.path = path
+        self.processed_datasets_dir = path / constants.PANDDA_PROCESSED_DATASETS_DIR
+        self.processed_datasets = {}
+        for dtag, dataset_dir in pandda_input.dataset_dirs.items():
+            processed_dataset_dir = dataset_dir.path / dtag
+            os.mkdir(processed_dataset_dir)
+            self.processed_datasets[dtag] = processed_dataset_dir
