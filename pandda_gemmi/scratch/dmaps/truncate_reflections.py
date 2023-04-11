701f7fc73633e38885ed6df654f69970361a5d7a
jfi22496 <conor.wild@sky.com>
2023-04-11 19:24:14 +0100

    New pandda

--- pandda_gemmi/scratch/dmaps/truncate_reflections.py
+++ pandda_gemmi/scratch/dmaps/truncate_reflections.py
@@ -188,7 +188,7 @@ def truncate_resolution(reflections, resolution: float):
     data_truncated = data[data["res"] >= resolution]
 
     # Rem,ove res colum
-    data_dropped = data_truncated.drop("res", "columns")
+    data_dropped = data_truncated.drop(columns=["res", "columns"])
 
     # To numpy
     data_dropped_array = data_dropped.to_numpy()
