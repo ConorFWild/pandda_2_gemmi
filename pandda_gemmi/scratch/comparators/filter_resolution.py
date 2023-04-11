1eae6c9d10bdb56f88ded7f4ea20f8af1486ffbc
jfi22496 <conor.wild@sky.com>
2023-04-11 15:12:06 +0100

    New pandda

--- pandda_gemmi/scratch/comparators/filter_resolution.py
+++ pandda_gemmi/scratch/comparators/filter_resolution.py
@@ -1,2 +1,15 @@
+from ..interfaces import *
+
 class FilterResolution:
-    ...
+    def __init__(self, resolution_cutoff: float):
+        self.resolution_cutoff = resolution_cutoff
+
+    def __call__(self, datasets: Dict[str, DatasetInterface], ):
+        high_resolution_dtags = filter(
+            lambda dtag: datasets[dtag].reflections.resolution() < self.resolution_cutoff,
+            datasets,
+        )
+
+        new_datasets = {dtag: datasets[dtag] for dtag in high_resolution_dtags}
+
+        return new_datasets
