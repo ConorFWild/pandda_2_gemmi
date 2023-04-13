30170147e075ecfeeadc30299cd167f90076e0d6
jfi22496 <conor.wild@sky.com>
2023-04-13 17:43:20 +0100

    New pandda

--- pandda_gemmi/scratch/pandda.py
+++ pandda_gemmi/scratch/pandda.py
@@ -210,6 +210,7 @@ def pandda(args: PanDDAArgs):
 
         # Get the dataset dmap
         dtag_index = np.argwhere(dtag_array == dtag)
+        print(dtag_index)
         dataset_dmap_array = dmaps[dtag_index, :]
         xmap_grid = reference_frame.unmask(SparseDMap(dataset_dmap_array))
 
