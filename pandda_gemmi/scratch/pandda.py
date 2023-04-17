7f225b487c6d6cb994f91cc902a1d50bb93c78a6
jfi22496 <conor.wild@sky.com>
2023-04-17 12:27:15 +0100

    P

--- pandda_gemmi/scratch/pandda.py
+++ pandda_gemmi/scratch/pandda.py
@@ -139,14 +139,14 @@ def pandda(args: PanDDAArgs):
     time_begin_process_datasets = time.time()
     _k = 0
     for dtag in datasets:
-        # _k += 1
-        # if _k >15:
-        #     continue
+        _k += 1
+        if _k >15:
+            continue
         # if dtag != "JMJD2DA-x427":
         #     continue
 
-        if dtag != "JMJD2DA-x348":
-            continue
+        # if dtag != "JMJD2DA-x348":
+        #     continue
         print(f"##### {dtag} #####")
         time_begin_process_dataset = time.time()
 
