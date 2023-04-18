9def07af8123c663929750a26b22791ee028bc95
jfi22496 <conor.wild@sky.com>
2023-04-18 15:31:56 +0100

    P

--- pandda_gemmi/scratch/event_model/score.py
+++ pandda_gemmi/scratch/event_model/score.py
@@ -2,6 +2,7 @@ import time
 
 import numpy as np
 import gemmi
+from scipy import optimize
 
 from ..interfaces import *
 
@@ -111,25 +112,42 @@ def get_bdc(event, xmap_grid, mean_grid, median):
     xmap_vals = xmap_array[event_indicies]
     mean_map_vals = mean_array[event_indicies]
     diffs = {}
-    for bdc in np.linspace(0.0, 0.95, 50):
-        # new_median = np.median((xmap_vals - (bdc * mean_map_vals)) / (1 - bdc))
-        # new_median = np.quantile(
-        #     (xmap_vals - (bdc * mean_map_vals)) / (1 - bdc),
-        #     np.linspace(0.05, 0.95, 10)
-        # )
-        new_median = np.quantile(
-            (xmap_vals - (bdc * mean_map_vals)) / (1 - bdc),
-            0.05
-        )
+    # for bdc in np.linspace(0.0, 0.95, 50):
+    #     # new_median = np.median((xmap_vals - (bdc * mean_map_vals)) / (1 - bdc))
+    #     # new_median = np.quantile(
+    #     #     (xmap_vals - (bdc * mean_map_vals)) / (1 - bdc),
+    #     #     np.linspace(0.05, 0.95, 10)
+    #     # )
+    #     new_median = np.quantile(
+    #         (xmap_vals - (bdc * mean_map_vals)) / (1 - bdc),
+    #         0.05
+    #     )
+    #
+    #     diff = np.abs(median - new_median)
+    #     diff_sum = float(np.sum(diff))
+    #     diffs[round(float(bdc), 2)] = diff_sum
+    #     # print(f"\t\t{round(float(bdc), 2)} : {round(float(median), 2)} {round(float(new_median), 2)} {round(diff, 2)}")
+    #     if dist < 5.0:
+    #         print(f"\t\t{round(float(bdc), 2)} : {round(float(np.mean(median)), 2)} {round(float(np.mean(new_median)), 2)} {round(diff_sum, 2)}")
+    #         # print(f"\t\t{diff}")
+    # return min(diffs, key=lambda _bdc: diffs[_bdc])
 
-        diff = np.abs(median - new_median)
-        diff_sum = float(np.sum(diff))
-        diffs[round(float(bdc), 2)] = diff_sum
-        # print(f"\t\t{round(float(bdc), 2)} : {round(float(median), 2)} {round(float(new_median), 2)} {round(diff, 2)}")
-        if dist < 5.0:
-            print(f"\t\t{round(float(bdc), 2)} : {round(float(np.mean(median)), 2)} {round(float(np.mean(new_median)), 2)} {round(diff_sum, 2)}")
-            # print(f"\t\t{diff}")
-    return min(diffs, key=lambda _bdc: diffs[_bdc])
+    res = optimize.minimize(
+        lambda _bdc: np.abs(
+            np.median(
+                (xmap_vals - (_bdc * mean_map_vals)) / (1 - _bdc)
+            ) - median
+        ),
+        0.5,
+        bounds=((0.0, 0.95),),
+        # tol=0.1
+    )
+    # print(res.x)
+    if dist < 5.0:
+        print(f"\t\t{round(float(res.x), 2)}")
+
+    return float(res.x)
+
 
 
 class ScoreCNN:
@@ -152,99 +170,14 @@ class ScoreCNN:
 
         self.n = n
 
-    def __call__(self, events, xmap_grid, mean_grid, z_grid, model_grid):
-
-        scored_events = {}
-        time_begin_get_images = time.time()
-        images = {}
-        for event_id, event in events.items():
-            centroid = np.mean(event.pos_array, axis=0)
-
-            sample_transform = get_sample_transform_from_event(
-                centroid,
-                0.5,
-                self.n,
-                3.5
-            )
-
-            sample_array = np.zeros((self.n, self.n, self.n), dtype=np.float32)
-
-            bdcs = np.linspace(0.0, 0.95, 20).reshape((20, 1, 1, 1))
-
-            xmap_sample = sample_xmap(xmap_grid, sample_transform, np.copy(sample_array))
-
-            mean_map_sample = sample_xmap(mean_grid, sample_transform, np.copy(sample_array))
-
-            image_events = (xmap_sample[np.newaxis, :] - (bdcs * mean_map_sample[np.newaxis, :])) / (1 - bdcs)
-
-            image_raw = np.stack([xmap_sample for _j in range(20)])
-
-            sample_array_zmap = np.copy(sample_array)
-            zmap_sample = sample_xmap(z_grid, sample_transform, sample_array_zmap)
-            image_zmap = np.stack([zmap_sample for _j in range(20)])
-
-            sample_array_model = np.copy(sample_array)
-
-            model_sample = sample_xmap(model_grid, sample_transform, sample_array_model)
-            image_model = np.stack([model_sample for _j in range(20)])
-
-            image = np.stack([image_events, image_raw, image_zmap, image_model], axis=1)
-            images[event_id] = image
-
-        time_finish_get_images = time.time()
-        print(f"\t\t\t\tGot images in: {round(time_finish_get_images-time_begin_get_images, 2)}")
-
-        for event_id, event in events.items():
-            image = images[event_id]
-
-            # Transfer to tensor
-            image_t = torch.from_numpy(image)
-
-            # Move tensors to device
-            image_c = image_t.to(self.dev)
-
-            # Run model
-            model_annotation = self.cnn(image_c.float())
-
-            # Track score
-            model_annotations = model_annotation.to(torch.device("cpu")).detach().numpy()
-
-            flat_bdcs = bdcs.flatten()
-            max_score_index = np.argmax([annotation for annotation in model_annotations[:, 1]])
-            bdc = float(flat_bdcs[max_score_index])
-            score = float(model_annotations[max_score_index, 1])
-
-            centroid = np.mean(event.pos_array, axis=0)
-            dist = np.linalg.norm(centroid - [6.0, -4.0, 25.0])
-            if dist < 5.0:
-                print(f"##### {event_id} #####")
-                for bdc, score in zip([x for x in flat_bdcs], [x for x in model_annotations[:, 1]]):
-                    print(f"\t\t\t{round(float(bdc), 2)} : {round(float(score), 2)}")
-
-            scored_event = Event(
-                event.pos_array,
-                event.point_array,
-                score,
-                bdc
-            )
-            scored_events[event_id] = scored_event
-
-
-        return scored_events
-
-    # def __call__(self, events, xmap_grid, mean_grid, z_grid, model_grid, median):
+    # def __call__(self, events, xmap_grid, mean_grid, z_grid, model_grid):
     #
     #     scored_events = {}
     #     time_begin_get_images = time.time()
     #     images = {}
-    #     bdcs = {}
     #     for event_id, event in events.items():
     #         centroid = np.mean(event.pos_array, axis=0)
-    #         dist = np.linalg.norm(centroid - [6.0, -4.0, 25.0])
-    #         if dist < 5.0:
-    #             print(f"##### {event_id} #####")
-    #             print(f"Centroid: {centroid}")
-    #             print(f"Distance: {dist}")
+    #
     #         sample_transform = get_sample_transform_from_event(
     #             centroid,
     #             0.5,
@@ -252,40 +185,32 @@ class ScoreCNN:
     #             3.5
     #         )
     #
-    #         bdc = get_bdc(event, xmap_grid, mean_grid, median)
-    #         bdcs[event_id] = bdc
-    #         # print(f"BDC: {bdc}")
-    #
     #         sample_array = np.zeros((self.n, self.n, self.n), dtype=np.float32)
     #
-    #         # bdcs = np.linspace(0.0, 0.95, 20).reshape((20, 1, 1, 1))
+    #         bdcs = np.linspace(0.0, 0.95, 20).reshape((20, 1, 1, 1))
     #
     #         xmap_sample = sample_xmap(xmap_grid, sample_transform, np.copy(sample_array))
     #
     #         mean_map_sample = sample_xmap(mean_grid, sample_transform, np.copy(sample_array))
     #
-    #         sample_event = (xmap_sample - (bdc * mean_map_sample)) / (1 - bdc)
-    #         image_event = sample_event[np.newaxis, :]
-    #         image_raw = xmap_sample[np.newaxis, :]
+    #         image_events = (xmap_sample[np.newaxis, :] - (bdcs * mean_map_sample[np.newaxis, :])) / (1 - bdcs)
     #
-    #         # image_raw = np.stack([xmap_sample for _j in range(20)])
+    #         image_raw = np.stack([xmap_sample for _j in range(20)])
     #
     #         sample_array_zmap = np.copy(sample_array)
     #         zmap_sample = sample_xmap(z_grid, sample_transform, sample_array_zmap)
-    #         image_zmap = zmap_sample[np.newaxis, :]
-    #         # image_zmap = np.stack([zmap_sample for _j in range(20)])
+    #         image_zmap = np.stack([zmap_sample for _j in range(20)])
     #
     #         sample_array_model = np.copy(sample_array)
     #
     #         model_sample = sample_xmap(model_grid, sample_transform, sample_array_model)
-    #         image_model = model_sample[np.newaxis, :]
-    #         # image_model = np.stack([model_sample for _j in range(20)])
+    #         image_model = np.stack([model_sample for _j in range(20)])
     #
-    #         image = np.stack([image_event, image_raw, image_zmap, image_model], axis=1)
+    #         image = np.stack([image_events, image_raw, image_zmap, image_model], axis=1)
     #         images[event_id] = image
     #
     #     time_finish_get_images = time.time()
-    #     # print(f"\t\t\t\tGot images in: {round(time_finish_get_images - time_begin_get_images, 2)}")
+    #     print(f"\t\t\t\tGot images in: {round(time_finish_get_images-time_begin_get_images, 2)}")
     #
     #     for event_id, event in events.items():
     #         image = images[event_id]
@@ -302,17 +227,110 @@ class ScoreCNN:
     #         # Track score
     #         model_annotations = model_annotation.to(torch.device("cpu")).detach().numpy()
     #
-    #         # flat_bdcs = bdcs.flatten()
+    #         flat_bdcs = bdcs.flatten()
     #         max_score_index = np.argmax([annotation for annotation in model_annotations[:, 1]])
-    #         # bdc = float(flat_bdcs[max_score_index])
+    #         bdc = float(flat_bdcs[max_score_index])
     #         score = float(model_annotations[max_score_index, 1])
+    #
+    #         centroid = np.mean(event.pos_array, axis=0)
+    #         dist = np.linalg.norm(centroid - [6.0, -4.0, 25.0])
+    #         if dist < 5.0:
+    #             print(f"##### {event_id} #####")
+    #             for bdc, score in zip([x for x in flat_bdcs], [x for x in model_annotations[:, 1]]):
+    #                 print(f"\t\t\t{round(float(bdc), 2)} : {round(float(score), 2)}")
     #
     #         scored_event = Event(
     #             event.pos_array,
     #             event.point_array,
     #             score,
-    #             bdcs[event_id]
+    #             bdc
     #         )
     #         scored_events[event_id] = scored_event
     #
+    #
     #     return scored_events
+
+    def __call__(self, events, xmap_grid, mean_grid, z_grid, model_grid, median):
+
+        scored_events = {}
+        time_begin_get_images = time.time()
+        images = {}
+        bdcs = {}
+        for event_id, event in events.items():
+            centroid = np.mean(event.pos_array, axis=0)
+            dist = np.linalg.norm(centroid - [6.0, -4.0, 25.0])
+            if dist < 5.0:
+                print(f"##### {event_id} #####")
+                print(f"Centroid: {centroid}")
+                print(f"Distance: {dist}")
+            sample_transform = get_sample_transform_from_event(
+                centroid,
+                0.5,
+                self.n,
+                3.5
+            )
+
+            bdc = get_bdc(event, xmap_grid, mean_grid, median)
+            bdcs[event_id] = bdc
+            # print(f"BDC: {bdc}")
+
+            sample_array = np.zeros((self.n, self.n, self.n), dtype=np.float32)
+
+            # bdcs = np.linspace(0.0, 0.95, 20).reshape((20, 1, 1, 1))
+
+            xmap_sample = sample_xmap(xmap_grid, sample_transform, np.copy(sample_array))
+
+            mean_map_sample = sample_xmap(mean_grid, sample_transform, np.copy(sample_array))
+
+            sample_event = (xmap_sample - (bdc * mean_map_sample)) / (1 - bdc)
+            image_event = sample_event[np.newaxis, :]
+            image_raw = xmap_sample[np.newaxis, :]
+
+            # image_raw = np.stack([xmap_sample for _j in range(20)])
+
+            sample_array_zmap = np.copy(sample_array)
+            zmap_sample = sample_xmap(z_grid, sample_transform, sample_array_zmap)
+            image_zmap = zmap_sample[np.newaxis, :]
+            # image_zmap = np.stack([zmap_sample for _j in range(20)])
+
+            sample_array_model = np.copy(sample_array)
+
+            model_sample = sample_xmap(model_grid, sample_transform, sample_array_model)
+            image_model = model_sample[np.newaxis, :]
+            # image_model = np.stack([model_sample for _j in range(20)])
+
+            image = np.stack([image_event, image_raw, image_zmap, image_model], axis=1)
+            images[event_id] = image
+
+        time_finish_get_images = time.time()
+        # print(f"\t\t\t\tGot images in: {round(time_finish_get_images - time_begin_get_images, 2)}")
+
+        for event_id, event in events.items():
+            image = images[event_id]
+
+            # Transfer to tensor
+            image_t = torch.from_numpy(image)
+
+            # Move tensors to device
+            image_c = image_t.to(self.dev)
+
+            # Run model
+            model_annotation = self.cnn(image_c.float())
+
+            # Track score
+            model_annotations = model_annotation.to(torch.device("cpu")).detach().numpy()
+
+            # flat_bdcs = bdcs.flatten()
+            max_score_index = np.argmax([annotation for annotation in model_annotations[:, 1]])
+            # bdc = float(flat_bdcs[max_score_index])
+            score = float(model_annotations[max_score_index, 1])
+
+            scored_event = Event(
+                event.pos_array,
+                event.point_array,
+                score,
+                bdcs[event_id]
+            )
+            scored_events[event_id] = scored_event
+
+        return scored_events
