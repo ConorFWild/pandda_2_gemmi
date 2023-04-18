3f4cc6f994d0c90273b3deed36507d0852df0b0d
jfi22496 <conor.wild@sky.com>
2023-04-18 15:03:30 +0100

    P

--- pandda_gemmi/scratch/event_model/output.py
+++ pandda_gemmi/scratch/event_model/output.py
@@ -44,6 +44,7 @@ def output_maps(
         dist = np.linalg.norm(centroid - [6.0, -4.0, 25.0])
         if dist < 5.0:
             print(f"##### {event_id} #####")
+            print(centroid)
             xmap_grid = reference_frame.unmask(SparseDMap(dtag_array))
             xmap_array = np.array(xmap_grid, copy=False)
             mean_array = np.array(mean_grid, copy=False)
