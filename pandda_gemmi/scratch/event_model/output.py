a843a9a667df12a89259c53792ede2649ced125f
jfi22496 <conor.wild@sky.com>
2023-04-18 15:18:23 +0100

    P

--- pandda_gemmi/scratch/event_model/output.py
+++ pandda_gemmi/scratch/event_model/output.py
@@ -38,6 +38,8 @@ def output_maps(
     save_dmap(mean_grid, fs.output.processed_datasets[dtag] / constants.PANDDA_MEAN_MAP_FILE.format(dtag=dtag))
 
     mean_inner_vals = selected_mean[reference_frame.mask.indicies_sparse_inner_atomic]
+    med = np.median(mean_inner_vals)
+    from scipy import optimize
 
     for event_id, event in selected_events.items():
         centroid = np.mean(event.pos_array, axis=0)
@@ -58,9 +60,23 @@ def output_maps(
 
             xmap_vals = xmap_array[event_indicies]
             mean_map_vals = mean_array[event_indicies]
-            fig, axs = plt.subplots(20, 1, figsize=(6, 20*2))
+            fig, axs = plt.subplots(21, 1, figsize=(6, 21*2))
 
-            for j, bdc in enumerate([x for x in np.linspace(0.0,0.95, 20)]):
+
+
+            res = optimize.minimize(
+                lambda _bdc: np.abs(
+                    np.median(
+                        (xmap_vals - (_bdc * mean_map_vals)) / (1 - _bdc)
+                    ) - med
+            ),
+                0.5,
+                bounds=((0.0, 0.95),),
+                # tol=0.1
+            )
+            print(res.x)
+
+            for j, bdc in enumerate([x for x in np.linspace(0.0,0.95, 20)] + [res.x,]):
                 bdc = round(float(bdc), 2)
                 event_map_vals = (xmap_vals - (bdc * mean_map_vals)) / (1 - bdc)
                 table = pd.DataFrame([
