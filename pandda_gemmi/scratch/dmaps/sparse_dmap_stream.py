f61f26ab64baba3191f064ca1d774b03b8aac146
jfi22496 <conor.wild@sky.com>
2023-04-19 11:55:00 +0100

    “

--- pandda_gemmi/scratch/dmaps/sparse_dmap_stream.py
+++ pandda_gemmi/scratch/dmaps/sparse_dmap_stream.py
@@ -120,8 +120,8 @@ class SparseDMapStream:
         #     for residue_id in dframe.partitioning.partitions
         # ]
         transforms, com_ref, com_mov = alignment.get_transforms()
-        print(f"Partitions: {[key for key in dframe.partitioning.partitions]}")
-        print(f"Transforms: {[key for key in transforms]}")
+        # print(f"Partitions: {[key for key in dframe.partitioning.partitions]}")
+        # print(f"Transforms: {[key for key in transforms]}")
 
         transform_list = [transforms[residue_id] for residue_id in dframe.partitioning.partitions ]
         com_moving_list = [com_mov[residue_id].tolist() for residue_id in dframe.partitioning.partitions ]
