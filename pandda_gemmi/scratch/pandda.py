df8272610a085736c065e427a19abce1a5fff434
jfi22496 <conor.wild@sky.com>
2023-05-19 14:38:40 +0100

    “

--- pandda_gemmi/scratch/pandda.py
+++ pandda_gemmi/scratch/pandda.py
@@ -362,7 +362,7 @@ def pandda(args: PanDDAArgs):
                     key=lambda _event_id: selected_model_events[_event_id].score,
                     reverse=True,
                 )
-            )[:5]
+            )[:3]
         }
 
         for event_id, event in top_selected_model_events.items():
