f0d2aa1dc197a9482345566d1435c899041cf89f
jfi22496 <conor.wild@sky.com>
2023-08-01 12:15:57 +0100

    Created output yamls for human readable input, model, event, autobuild data

--- scripts/pandda_review.py
+++ scripts/pandda_review.py
@@ -3,6 +3,7 @@ import argparse
 
 import streamlit as st
 import pandas as pd
+from matplotlib import pyplot as plt
 
 
 def __main__():
@@ -21,6 +22,10 @@ def __main__():
 
     table = pd.read_csv(pandda_inspect_events)
     st.line_chart(data=table, x=None, y="z_peak")
+
+    fig = plt.scatter(x=table["z_peak"], y=table["cluster_size"])
+    st.pyplot(fig)
+
     st.write(table)
 
 
