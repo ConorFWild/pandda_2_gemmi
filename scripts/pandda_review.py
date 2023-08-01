10264f97bfdfaeaa98485a73bbc2e5270998f44f
jfi22496 <conor.wild@sky.com>
2023-08-01 14:21:10 +0100

    Created output yamls for human readable input, model, event, autobuild data

--- scripts/pandda_review.py
+++ scripts/pandda_review.py
@@ -18,17 +18,26 @@ def __main__():
 
     st.write(pandda_inspect_events)
 
-
-
     table = pd.read_csv(pandda_inspect_events)
-    st.line_chart(data=table, x=None, y="z_peak",)
+    st.line_chart(data=table, x=None, y="z_peak", )
 
     fig = plt.figure()
     plt.scatter(x=table["z_peak"], y=table["cluster_size"], s=0.1)
     plt.yscale("log")
     st.pyplot(fig)
 
+    event_ids = []
+    for _idx, _row in table.iterrows():
+        dtag, event_idx = _row["dtag"], _row["event_idx"]
+        event_ids.append((dtag, event_idx))
+    option = st.selectbox("", event_ids)
+
+    if option is not None:
+        st.write(f"{option[0]} : {option[1]}")
+
     st.write(table)
+
+
 
 
 if __name__ == "__main__":
