b0eeba47a2b1622d5dd89251f5ee3c34a1a6b47f
ConorFWild <41680328+ConorFWild@users.noreply.github.com>
2023-10-12 10:27:36 +0100

    Devcontainer setup

--- pandda_gemmi/event_model/filter.py
+++ pandda_gemmi/event_model/filter.py
@@ -208,6 +208,8 @@ class FilterSymmetryPosBuilds:
                                 if mark_pos.dist(original_atom_pos) > 0.0001:
                                     dists.append(mark_pos.dist(atom_pos))
 
+            print(f"Distances: {dists}")
+
             # Don't filter no sym atoms near
             if len(dists) == 0:
                 new_events[event_id] = event
