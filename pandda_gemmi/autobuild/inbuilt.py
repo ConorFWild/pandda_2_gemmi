75ac6d987cf87e19397ed8709cb6afd3c6c974e9
ConorFWild <41680328+ConorFWild@users.noreply.github.com>
2023-10-16 14:56:14 +0100

    Fix for ACEDRG

--- pandda_gemmi/autobuild/inbuilt.py
+++ pandda_gemmi/autobuild/inbuilt.py
@@ -856,10 +856,10 @@ def get_local_signal(optimized_structure, event_map_grid):
                     #                                   radius=1.5,
                     #                                   value=0,
                     #                                   )
-                    inner_mask_grid.set_points_around(pos,
-                                                      radius=1.0,
-                                                      value=2,
-                                                      )
+                    # inner_mask_grid.set_points_around(pos,
+                    #                                   radius=1.0,
+                    #                                   value=2,
+                    #                                   )
                     inner_mask_grid.set_points_around(pos,
                                                       radius=0.5,
                                                       value=3,
