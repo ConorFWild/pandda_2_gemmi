ad683ac585ab411fe5384bcbf37fa55fa701c615
ConorFWild <41680328+ConorFWild@users.noreply.github.com>
2023-10-16 16:52:42 +0100

    Fix for ACEDRG

--- pandda_gemmi/autobuild/inbuilt.py
+++ pandda_gemmi/autobuild/inbuilt.py
@@ -877,6 +877,7 @@ def get_local_signal(optimized_structure, event_map_grid):
 
     core_points = event_map_grid_array[np.nonzero(inner_mask_grid_array == 3)]
     high_core = np.sum(core_points > background)
+
     score = high_core-(high_non_core*(core_points.size / outer_mask.size))
 
     # return np.sum(vals_pos-np.mean(vals_neg)) #- np.sum(vals_neg)
