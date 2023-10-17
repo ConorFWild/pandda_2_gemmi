34413fc5552e7438ece4271e9e5c190645092dd3
ConorFWild <41680328+ConorFWild@users.noreply.github.com>
2023-10-17 17:00:43 +0100

    p

--- pandda_gemmi/autobuild/inbuilt.py
+++ pandda_gemmi/autobuild/inbuilt.py
@@ -839,7 +839,7 @@ def get_local_signal_dencalc(optimized_structure, event_map_grid, res, ):
     optimized_structure.cell = event_map_grid.unit_cell
     optimized_structure.spacegroup_hm = gemmi.find_spacegroup_by_name("P 1").hm
     dencalc = gemmi.DensityCalculatorE()
-    dencalc.d_min = res
+    dencalc.d_min = res*2
     dencalc.rate = 4.0
     # initial_dencalc_grid = gemmi.FloatGrid(event_map_grid.nu, event_map_grid.nv, event_map_grid.nw)
     # initial_dencalc_grid.spacegroup = gemmi.find_spacegroup_by_name("P 1")
