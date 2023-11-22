76de590a516dfa9d23d6a549d4826c18706be26f
ConorFWild <41680328+ConorFWild@users.noreply.github.com>
2023-11-22 14:54:07 +0000

    Change symmetry masking for fix to experimenting on Mpro-x0336

--- pandda_gemmi/event_model/filter_selected_events.py
+++ pandda_gemmi/event_model/filter_selected_events.py
@@ -6,8 +6,9 @@ def filter_selected_events(dtag, selected_events, ):
         in list(
             sorted(
                 selected_model_events,
+                key=lambda _event_id: selected_model_events[_event_id].score,
                 # key=lambda _event_id: selected_model_events[_event_id].build.score,
-                key=lambda _event_id: selected_model_events[_event_id].local_strength * (selected_model_events[_event_id].build.signal / selected_model_events[_event_id].build.noise),
+                # key=lambda _event_id: selected_model_events[_event_id].local_strength * (selected_model_events[_event_id].build.signal / selected_model_events[_event_id].build.noise),
                 reverse=True,
             )
         )[:3]
