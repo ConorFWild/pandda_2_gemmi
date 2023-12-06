Change symmetry masking for fix to experimenting on Mpro-x0336

scripts/all_tests.py
@@ -488,13 +488,14 @@ def get_masked_dmap(dmap, st):
 
     # Get the mask array
     mask_array = np.array(mask, copy=False)
-    print(np.sum(mask_array))
 
     # Get the dmap array
     dmap_array = np.array(dmap, copy=False)
 
     # Mask the dmap array
     dmap_array[mask_array == 0] = 0.0
+    print(np.sum(mask_array))
+
 
     return dmap
 