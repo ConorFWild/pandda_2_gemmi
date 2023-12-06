Change symmetry masking for fix to experimenting on Mpro-x0336

scripts/all_tests.py
@@ -502,7 +502,6 @@ def score_build(autobuilt_structure, event_map,
                 raw_xmap,
                 model, dev):
     # Get the masked event map
-    masked_event_map = get_masked_dmap(event_map, autobuilt_structure)
 
     # Get the ligand centroid
     centroid = get_centroid(autobuilt_structure)
@@ -518,10 +517,12 @@ def score_build(autobuilt_structure, event_map,
 
     # Event map sample
     dmap_sample_array = np.copy(sample_array)
+    masked_event_map = get_masked_dmap(event_map, autobuilt_structure)
     dmap_sample = sample_xmap(masked_event_map, sample_transform, dmap_sample_array)
     dmap_mean = np.mean(dmap_sample)
     dmap_std = np.std(dmap_sample)
     if np.abs(dmap_std) < 0.0000001:
+        print(f"Warning! Bad Event Map Sample!")
         image_dmap = np.copy(sample_array)[np.newaxis, :]
     else:
         image_dmap = (dmap_sample[np.newaxis, :] - dmap_mean) / dmap_std
@@ -533,6 +534,7 @@ def score_build(autobuilt_structure, event_map,
     zmap_mean = np.mean(zmap_sample)
     zmap_std = np.std(zmap_sample)
     if np.abs(zmap_std) < 0.0000001:
+        print(f"Warning! Bad ZMap Sample!")
         image_zmap = np.copy(sample_array)[np.newaxis, :]
     else:
         image_zmap = (zmap_sample[np.newaxis, :] - zmap_mean) / zmap_std
@@ -544,6 +546,7 @@ def score_build(autobuilt_structure, event_map,
     xmap_mean = np.mean(xmap_sample)
     xmap_std = np.std(xmap_sample)
     if np.abs(xmap_std) < 0.0000001:
+        print(f"Warning! Bad XMap Sample!")
         image_raw_xmap = np.copy(sample_array)[np.newaxis, :]
     else:
         image_raw_xmap = (xmap_sample[np.newaxis, :] - xmap_mean) / xmap_std