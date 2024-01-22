b7a8a4a8c0339ca47d887b4589f4184434911dce
ConorFWild <41680328+ConorFWild@users.noreply.github.com>
2024-01-22 11:21:57 +0000

    Rank events by site by default

--- pandda_gemmi/comparators/filter_r_free.py
+++ pandda_gemmi/comparators/filter_r_free.py
@@ -2,18 +2,19 @@ from ..interfaces import *
 
 
 class FilterRFree:
-    def __init__(self, high_res_limit_lower: float):
-        self.high_res_limit_lower = high_res_limit_lower
+    def __init__(self, max_rfree: float):
+        self.max_rfree = max_rfree
 
     def __call__(self, datasets: Dict[str, DatasetInterface]):
-        good_res_dtags = filter(
-            lambda dtag: datasets[dtag].reflections.resolution() < self.high_res_limit_lower,
+        good_rfree_dtags = filter(
+            lambda dtag: datasets[dtag].structure.rfree() < self.max_rfree,
             datasets,
         )
 
-        new_datasets = {dtag: datasets[dtag] for dtag in good_res_dtags}
+        new_datasets = {dtag: datasets[dtag] for dtag in good_rfree_dtags}
+
 
         return new_datasets
 
     def description(self):
-        return f"Filtered because resolution > {self.high_res_limit_lower}"
+        return f"Filtered because rfree > {self.max_rfree}"
