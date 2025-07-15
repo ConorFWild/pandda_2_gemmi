06c47dd715a0fc34ec0f408540ac23387688d98c
ConorFWild <41680328+ConorFWild@users.noreply.github.com>
2025-07-15 15:25:44 +0100

    TSNE fallback for projections

--- pandda_gemmi/comparators/get_comparators.py
+++ pandda_gemmi/comparators/get_comparators.py
@@ -1,8 +1,10 @@
 from ..interfaces import *
 
-def get_comparators(datasets: Dict[str, DatasetInterface], filters):
+def get_comparators(datasets: Dict[str, DatasetInterface], filters, debug=False):
 
     for filter in filters:
         datasets = filter(datasets)
+        if debug
+            print(f'{type(filter)} : {[x for x in datasets]}')
 
     return datasets
