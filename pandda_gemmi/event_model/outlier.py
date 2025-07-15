8bc0fd99118fe3ae54147375636a80a26ff2b4e6
ConorFWild <41680328+ConorFWild@users.noreply.github.com>
2025-07-15 15:35:49 +0100

    s

--- pandda_gemmi/event_model/outlier.py
+++ pandda_gemmi/event_model/outlier.py
@@ -14,8 +14,10 @@ class PointwiseMAD:
     def __call__(self,
                  dataset_dmap_array,
                  characterization_set_dmaps_array,
+                 std_floor=0.1
                  ):
         mean = np.median(characterization_set_dmaps_array, axis=0)
         std = np.median(np.abs(characterization_set_dmaps_array-mean), axis=0) / 0.6745
+        std[std < std_floor] = std_floor
         z = ((dataset_dmap_array - mean) / std)
         return mean, std, z
